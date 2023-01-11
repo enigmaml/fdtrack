27d9147f6ea666d768a974cb5fa86757e20a0492
enigmaml <enigma.pythonml@gmail.com>
2023-01-11 14:20:31 +0530

    basic structure

--- app.py
+++ app.py
@@ -1,35 +1,51 @@
 import streamlit as st
 
+  
+import gspread 
+
+import pandas as pd
+
+sa = gspread.service_account(filename='authcred.json')
+sh = sa.open("fddetails")
+
+wks = sh.worksheet("Sheet1")
+# print(wks.get_all_records())
+
+df=pd.DataFrame(wks.get_all_records())
 
 st.title("FD App")
-st.subheader("Papa")
 
-st.header("Papa")
+st.write("## Enter FD Details")
 
+fdForOptions=['','PAPA','MUMMY','MANOJ','ANU','KANCHU']
 
-st.text('''People love to hate on pigeons for the way they foul up parked cars or flock 
-to food scraps on the sidewalk. But with more than 300 species of wild pigeon 
-found on Earth—many of them stunning—it’s past time the lowly pigeon gets its coo.''')
+fdForName=st.selectbox("FD For", fdForOptions)
 
+initialValue=st.text_input("Enter Initial Value")
+
+interestRate=st.text_input("Enter Interest Rate")
+
+maturityValue=st.text_input("Enter Maturity Value")
+
+bankOptions=['','Utkarsk Bank']
+
+bankName=st.selectbox("Select Bank", bankOptions)
+
+fdOpeingdate=st.date_input("Select Opeing date")
+
+fdMaturitydate=st.date_input("Select Maturity date")
+
+fdNo=st.text_input("Enter FD No")
+
+result=st.button('SAVE')
+
+st.markdown("---")
 
 #https://www.markdownguide.org/cheat-sheet/
 st.markdown(">Papa")
 
-st.caption("Hello")
+st.table(df)
 #https://katex.org/docs/supported.html
-st.latex(r"\begin{pmatrix}   a & b \\   c & d\end{pmatrix}")
-
-a={"name":"Manoj" , "age":34}
-st.json(a)
-
-
-code='''
-
-print('Hello World!!')
-'''
-
-st.code(code)
 
 
 
-st.code(code,language='python')
